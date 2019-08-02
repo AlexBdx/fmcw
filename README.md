0bb6dfa421f4299bc7d9cb426bc9b06b90f48f1f
AlexBdx <alexandre.bdx@gmail.com>
2019-08-02 14:36:33 -0400

    First upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# FMCW
+Based on initial work by Henrik Forsten
+Python 3.X implementation of the software layer handling the fmcw3 radar.
+Includes acquisition and post-processing scripts.
---
+++ __init__.py
@@ -0,0 +1,10 @@
+from os.path import dirname, basename, isfile, join, abspath, split
+import glob
+
+path_glob = join(dirname(abspath(__file__)), '*.py')
+modules = glob.glob(path_glob)
+#valid_modules = [mod for mod in modules if mod[-3:] == '.py']
+module_blacklist = ['__init__.py', 'setup.py']
+__name__ = "videotools"
+__version__ = "0.0.14"
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and split(f)[1] not in module_blacklist]
