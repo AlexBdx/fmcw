ba908fb153ce88808ae8efdb3c02b6770af99f12
AlexBdx <alexandre.bdx@gmail.com>
2019-08-19 07:15:06 -0400

    New attempt

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fmcw",
-    version="0.3.26",
+    version="0.3.27",
     author="Alex Bondoux",
     author_email="alexandre.bdx@gmail.com",
     description="Library for fmcw radar",
