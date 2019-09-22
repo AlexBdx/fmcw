03e63d7470f34f39d9964a2c22d516036dc06e66
AlexBdx <alexandre.bdx@gmail.com>
2019-09-22 12:49:45 -0400

    Folder reorg

--- fmcw/setup.py
+++ fmcw/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fmcw",
-    version="3.1.8",
+    version="3.1.9",
     author="Alex Bondoux",
     author_email="alexandre.bdx@gmail.com",
     description="Library for fmcw radar",
