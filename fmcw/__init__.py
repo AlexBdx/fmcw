fbd1c986fd9e9cd6e7f5669e3bf79b49445df298
AlexBdx <alexandre.bdx@gmail.com>
2019-09-22 13:34:24 -0400

    Folder reorg

--- fmcw/__init__.py
+++ fmcw/__init__.py
@@ -3,8 +3,10 @@ import glob
 
 path_glob = join(dirname(abspath(__file__)), '*.py')
 modules = glob.glob(path_glob)
-#valid_modules = [mod for mod in modules if mod[-3:] == '.py']
 module_blacklist = ['__init__.py', 'setup.py', '190903_rt_fmcw3.py']
+
 __name__ = "fmcw"
-__version__ = "3.1.18"
-__all__ = [ basename(f)[:-3] for f in modules if isfile(f) and split(f)[1] not in module_blacklist]
+__version__ = "3.1.19"
+__all__ = [basename(f)[:-3] for f in modules if isfile(f) and split(f)[1] not in module_blacklist]
+print(__all__)
+
